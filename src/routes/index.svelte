fc9502777348e45f8f8ffc3d6fb6c186caec9e36
Robin <19738286+cawc@users.noreply.github.com>
2020-12-14 00:23:16 +0100

    add random roll

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,14 @@
 <script>
-	function roll(){
+	export let idea = '...'
+	async function roll(){
 		console.log('rolling')
+		const response = await fetch(`http://localhost:5000/random`, {
+            method: "GET"
+		})
+		const ideaJson = await response.json()
+		idea = ideaJson.idea
 	}
+	
 </script>
 
 <svelte:head>
@@ -10,4 +17,4 @@
 
 <h1 class="title">idea app</h1>
 
-<p>Try ...</p> <button on:click={roll} class="button is-light" type="button">re-roll</button>
+<p>Try {idea}</p> <button on:click={roll} class="button is-light" type="button">re-roll</button>
